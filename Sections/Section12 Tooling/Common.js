b0cb6bbb6bd56ce31476393d83f3d400b899baa5
darecallad <Ashane199080706@hotmail.com.tw>
2021-05-18 17:52:10 -0700

    05-18-2021 5:52 PM

---
+++ Sections/Section12 Tooling/Common.js
@@ -0,0 +1,12 @@
+// Common JS Modules
+
+// Things are highly releate they should go together
+// Cohesion
+
+module.exports.Circle = Circle;
+module.exports = Circle;
+
+
+// import
+const Circle = require('./Modules');
+// Common wat to exports / import js files in ES6
--- Sections/Section12 Tooling/Modules.js
+++ Sections/Section12 Tooling/Modules.js
@@ -1,1 +1,4 @@
 // Modules 
+
+// Common JS  which use Node 
+// ES6 Modules which use browser
